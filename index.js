18da18763b1908a102ea0304aaaf71b59c41a171
Sofiezar Ricky <sofiezar195@gmail.com>
2019-12-04 22:11:45 +0700

    MidleWare Node JS PPT

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const path = require('path')
+const bodyParser = require('body-parser')
+
+const app = express()
+app.set('view engine', 'ejs')
+app.set('views', 'views')
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+app.get("/", (req, res) => {
+    res.render("login")
+})
+
+require('./router/router')(app)
+
+app.listen(3001, () => {
+    console.log("Berhasil menjalankan server pada port 3001");
+
+})
